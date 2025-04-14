db310da0ab0a1dce40f7cf228ac989070a5ac707
GitHub Action <action@github.com>
2025-04-14 17:24:28 +0000

    Update routes and content from process-chapter script

--- lib/routes-config.ts
+++ lib/routes-config.ts
@@ -1,33 +1,37 @@
 export type EachRoute = {
-  title: string;
-  href: string;
-  noLink?: true;
-  items?: EachRoute[];
-};
-
-export const ROUTES: EachRoute[] = [
+      title: string;
+      href: string;
+      noLink?: true;
+      items?: EachRoute[];
+    };
+    export const ROUTES: EachRoute[] = [
   {
-    title: "Chapters",
-    href: "/introduction",
-    noLink: true,
-    items: [
-      { title: "Introduction", href: "/" },
-    ],
-  }, 
-];
-
-type Page = { title: string; href: string };
-
-function getRecurrsiveAllLinks(node: EachRoute) {
-  const ans: Page[] = [];
-  if (!node.noLink) {
-    ans.push({ title: node.title, href: node.href });
+    "title": "Chapters",
+    "href": "/introduction",
+    "noLink": true,
+    "items": [
+      {
+        "title": "Introduction",
+        "href": "/introduction"
+      },
+      {
+        "title": "Introduction",
+        "href": "/"
+      }
+    ]
   }
-  node.items?.forEach((subNode) => {
-    const temp = { ...subNode, href: `${node.href}${subNode.href}` };
-    ans.push(...getRecurrsiveAllLinks(temp));
-  });
-  return ans;
-}
-
-export const page_routes = ROUTES.map((it) => getRecurrsiveAllLinks(it)).flat();
+];
+    type Page = { title: string; href: string };
+    function getRecurrsiveAllLinks(node: EachRoute) {
+      const ans: Page[] = [];
+      if (!node.noLink) {
+        ans.push({ title: node.title, href: node.href });
+      }
+      node.items?.forEach((subNode) => {
+        const temp = { ...subNode, href: `${node.href}${subNode.href}` };
+        ans.push(...getRecurrsiveAllLinks(temp));
+      });
+      return ans;
+    }
+    export const page_routes = ROUTES.map((it) => getRecurrsiveAllLinks(it)).flat();
+    
